42f80826b0e7f86bb9b7a0a2c553b2b936b48807
danielwalker <dawalker2017@outlook.com>
2019-09-22 20:52:30 -0400

    09.22.19 08:52pm Checkpoint_ValidationForm

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//----------------------------------------------------------------------------//
+//---------------------------------------------------------------------------------//
 
 function query(selector) { //assigned the selector of 'query' to a function.//
     return document.querySelector(selector)
@@ -8,139 +8,167 @@ function queryAll(selector) { //assigned the selector of 'queryAll' to a functio
     return document.querySelectorAll(selector)
 }
 
-//----------------------------------------------------------------------------//
-//----------------------------------------------------------------------------//
+function markValid(field) {
+    // created a function that, when seen as 'valid',
+    // removes 'invalid status' and adds 'valid status'.
+    field.classList.remove('input-invalid')
+    field.classList.add('input-valid')
+        // ** Attempted 'field.parentNode.classList...' here and failed
+        // due to Name Input having the parentNode of the entire form.
+        // So instead of validating Name Input only, it went to everything.**
+}
 
-//BEGIN NAME INPUT//
+function markInvalid(field, errorMessage) {
+    // created a function that, when seen as 'invalid',
+    // removes the 'valid' status and adds 'invalid' status.
+    field.classList.remove('input-valid')
+    field.classList.add('input-invalid')
 
-query('#parking-form').addEventListener('submit', function(event) {
-    //calls back the selector as declared above and selects 'id parking-form',
-    //then adds an eventListener of 'submit' to it.//
-    event.preventDefault()
-        //'preventDefault' prevents the 'default' action of the form.//
+    if (errorMessage) {
+        // "if" there are error messages...
 
-    let nameField = query('#name') //declaration of the input to a 'keyword'.//
-    let nameText = nameField.value.trim() //declaration that labels the outcome of the declared input above.//
+        let errorElement = document.createElement('p')
+            // ...then declare that 'error' creates a new paragraph element.
 
-    if (!nameText) { //creates the event that is "not" in the input.//
-        nameField.parentNode.classList.remove('input-valid')
-            //allows for change based on input being NOT valid... which means it is false.//
-        nameField.parentNode.classList.add('input-invalid')
-            //allows for change based on input being NOT invalid... which means it is true.//
-    } else {
-        nameField.parentNode.classList.remove('input-invalid')
-        nameField.parentNode.classList.add('input-valid')
-            //creates the opposite change of previous change above. 'IF' input is flase, change to invalid. 'ELSE' change it to valid.//
+        errorElement.classList.add('input-hint', 'text-danger', 'error-message')
+        errorElement.innerText = errorMessage
+        field.appendChild(errorElement)
+            // After element is created on 'errorElement' bind, 
+            // 'errorElement' adds messages to list of classes.
+            // Finally adding the paragraph containing the error message.
     }
+}
+
+function errorClear() {
+    // Created a function for clearing error messages.
+    let errorWipe = queryAll('.text-danger')
+        // 'errors' is now set to the Boot-strap default of 'red'.
+    for (let i = 0; i < errorWipe.length; i++) {
+        errorWipe[i].remove()
+    } // Created a for-loop in which, starting at 0, runs through the 
+    // selection until all of 'errors_value' is removed.
+}
+
+//---------------------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------------//NAME_INPUT//
+query('#parking-form').addEventListener('submit', function() {
+    event.preventDefault()
+        // Creating the function that adds and Event Listener of 'submit' to 
+        // the form. As well as preventing the default of refreshing the form 
+        // after 'submit' is clicked.
+
+    errorClear()
+        // Recalling the function to clear all the error messages.
+
+
+    let nameField = query('#name-field')
+    let name = query('#name').value
+        // Declaring all of the variables and thier 'pathways' for later use.
+
+    if (!name) {
+        markInvalid(nameField, 'Missing Required Information')
+    } else {
+        markValid(nameField)
+    }
+
+
+    //----------------------------------------------------------------------------//CAR_INPUT//in_progress...
+
+    let carField = query('#car-field')
+    let carYear = query('#car-year').value
+    let carMake = query('#car-make').value
+    let carModel = query('#car-model').value
+        // Declaring all of the variables and thier 'pathways' for later use.
+
+    if (!carYear || !carMake || !carModel) {
+        markInvalid(carField)
+    } else {
+        markValid(carField)
+            // Created an if/else statement with '||' equaling 'or', so that 
+            // the value of each input is required to continue.
+    }
+
+    if (!carYear) {
+        markInvalid(carField, 'Invalid Year')
+    }
+
+    if (!carMake) {
+        markInvalid(carField, 'Invalid Make')
+    }
+
+    if (!carModel) {
+        markInvalid(carField, 'Invalid Model')
+    }
+    // 
+
+    //----------------------------------------------------------------------------//DATE_PARKING_INPUT//
+
+    let dateField = query('#start-date-field')
+    let dateText = query('#start-date')
+    let date = dateText.value.trim()
+
+    if (!date) {
+        markInvalid(dateField, 'Invalid Date')
+    } else {
+        markValid(dateField)
+    }
+
+    //----------------------------------------------------------------------------//NUMBER_OF_DAYS_INPUT//
+
+
+    let daysField = query('#days-field')
+    let daysText = query('#days')
+    let days = daysText.value.trim()
+        // Declaring that 'daysField' binds to the input of '#days' 
+        // and days is equal to the value of said input.
+
+    if (!days) {
+        markInvalid(daysField, 'Missing Required Information')
+    } else {
+        markValid(daysField)
+    }
+    // Created 'if/else' stating that 'if' days input is empty, mark invalid
+    // or 'else' mark valid.
+
+
+    //----------------------------------------------------------------------------//CREDIT_CARD_INPUT//
+
+    let cardField = query('#credit-card-field')
+    let cardText = query('#credit-card')
+    let card = cardText.value.trim()
+
+    if (!card) {
+        markInvalid(cardField, 'Invalid Card Number')
+    } else {
+        markValid(cardField)
+    }
+
+    //----------------------------------------------------------------------------//CVV_INPUT//
+
+    let cvvField = query('#cvv-field')
+    let cvvText = query('#cvv')
+    let cvv = cvvText.value.trim()
+
+    if (!cvv) {
+        markInvalid(cvvField, 'Missing Valid CVV')
+    } else {
+        markValid(cvvField)
+    }
+
+    //----------------------------------------------------------------------------//EXPIRATION_INPUT//
+
+    let expField = query('#expiration-field')
+    let expText = query('#expiration')
+    let exp = expText.value.trim()
+
+    if (!exp) {
+        markInvalid(expField, 'Missing Valid Expiration')
+    } else {
+        markValid(expField)
+    }
+
 })
 
-//END NAME INPUT//
-
 //----------------------------------------------------------------------------//
-//----------------------------------------------------------------------------//
-
-//BEGIN CAR INPUT//
-
-query('#parking-form').addEventListener('submit', function() {
-
-            let yearField = query()
-
-        }
-
-        //END CAR INPUT//
-
-        //----------------------------------------------------------------------------//
-        //----------------------------------------------------------------------------//
-
-
-        //BEGIN DATE PARKING INPUT//
-
-        //END DATE PARKING INPUT//
-
-        //----------------------------------------------------------------------------//
-        //----------------------------------------------------------------------------//
-
-        //BEGIN NUMBER OF DAYS//
-
-        query('#parking-form').addEventListener('submit', function(event) {
-
-            let daysField = query('#days')
-            let daysText = daysField.value.trim()
-
-            if (!daysText) {
-                daysField.parentNode.classList.remove('input-valid')
-                daysField.parentNode.classList.add('input-invalid')
-
-            } else {
-                daysField.parentNode.classList.remove('input-invalid')
-                daysField.parentNode.classList.add('input-valid')
-            }
-        })
-
-        //END NUMBER OF DAYS INPUT//
-
-        //----------------------------------------------------------------------------//
-        //----------------------------------------------------------------------------//
-
-        //BEGIN CREDIT CARD INPUT//
-
-        query('#parking-form').addEventListener('submit', function(event) {
-
-            let cardField = query('#credit-card')
-            let cardText = cardField.value.trim()
-
-            if (!cardText) {
-                cardField.parentNode.classList.remove('input-valid')
-                cardField.parentNode.classList.add('input-invalid')
-            } else {
-                cardField.parentNode.classList.remove('input-invalid')
-                cardField.parentNode.classList.add('input-valid')
-            }
-        })
-
-        //END CREDIT CARD INPUT//
-
-        //----------------------------------------------------------------------------//
-        //----------------------------------------------------------------------------//
-
-        //BEGIN CVV INPUT//
-
-        query('#parking-form').addEventListener('submit', function(event) {
-
-            let cvvField = query('#cvv')
-            let cvvText = cvvField.value.trim()
-
-            if (!cvvText) {
-                cvvField.parentNode.classList.remove('input-valid')
-                cvvField.parentNode.classList.add('input-invalid')
-            } else {
-                cvvField.parentNode.classList.remove('input-invalid')
-                cvvField.parentNode.classList.add('input-valid')
-            }
-        })
-
-        //END CVV INPUT//
-
-        //----------------------------------------------------------------------------//
-        //----------------------------------------------------------------------------//
-
-        //BEGIN EXPIRATION INPUT//
-
-        query('#parking-form').addEventListener('submit', function() {
-
-            let expField = query('#expiration')
-            let expText = expField.value.trim()
-
-            if (!expText) {
-                expField.parentNode.classList.remove('input-valid')
-                expField.parentNode.classList.add('input-invalid')
-            } else {
-                expField.parentNode.classList.remove('input-invalid')
-                expField.parentNode.classList.add('input-valid')
-            }
-        })
-
-        //END EXPIRATION INPUT//
-
-        //----------------------------------------------------------------------------//
-        //----------------------------------------------------------------------------//
